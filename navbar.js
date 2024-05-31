b57fc872f4bcd87021d13fa7b3e3aca1868d4fdd
nitro207 <128526025+nitro207@users.noreply.github.com>
2024-05-31 01:17:51 -0700

    created base

--- navbar.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Fetch and insert the content of the navbar.html into the "navbarContainer" div
-    fetch("navbar.html")
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById("navbarContainer").innerHTML = html;
-        });
-});
