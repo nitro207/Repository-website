f71639b583f0c189fe7ae541ec471668771481e2
nitro207 <128526025+nitro207@users.noreply.github.com>
2024-06-09 20:55:16 -0700

    added foote

---
+++ footer.js
@@ -0,0 +1,7 @@
+
+    function github(){
+        window.open('https://github.com/nitro207','_blank');
+    }
+    function linkedin(){
+        window.open('https://www.linkedin.com/in/anthony-scott-059296267/','_blank');
+    }
